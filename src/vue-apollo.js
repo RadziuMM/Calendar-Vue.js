5d7c455a367d0c8452286fc388d641f3e09cec8f
Radoslaw Mazurczak <RMazurczak@protonmail.com>
2020-09-03 21:45:34 +0200

    db connection

--- src/vue-apollo.js
+++ src/vue-apollo.js
@@ -9,7 +9,7 @@ Vue.use(VueApollo);
 const AUTH_TOKEN = 'apollo-token';
 
 // Http endpoint
-const httpEndpoint = process.env.VUE_APP_GRAPHQL_HTTP || 'http://localhost:4000/graphql';
+const httpEndpoint = process.env.VUE_APP_GRAPHQL_HTTP || 'https://calendarsss.herokuapp.com/';
 
 // Config
 const defaultOptions = {
