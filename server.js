fea2542dd59a72d5b3b70a794533c771009d04ba
Radoslaw Mazurczak <RMazurczak@protonmail.com>
2020-11-05 13:57:30 +0100

    merge fix#2

--- server.js
+++ server.js
@@ -1,19 +1,16 @@
-const express = require("express");
-const serveStatic = require("serve-static");
-const path = require("path");
-const { ApolloServer, gql } = require("apollo-server");
-const { readFileSync } = require("fs");
-const { prisma } = require("./src/src/generated/prisma-client");
+const express = require('express');
+const serveStatic = require('serve-static');
+const path = require('path');
+const { ApolloServer, gql } = require('apollo-server-express');
+const { readFileSync } = require('fs');
+const { prisma } = require('./src/src/generated/prisma-client');
 
 const resolvers = {
   Query: {
+    // eslint-disable-next-line no-return-await
     users: async () => await prisma.users(),
-    user: async (parent, args) => {
-      return prisma.user({ id: args.id });
-    },
-    userBYname: async (parent, args) => {
-      return prisma.user({ name: args.name });
-    },
+    user: async (parent, args) => prisma.user({ id: args.id }),
+    userBYname: async (parent, args) => prisma.user({ name: args.name }),
   },
   Mutation: {
     createUser: async (parent, args) => {
@@ -21,26 +18,26 @@ const resolvers = {
         name: args.user.name,
         exist: true,
         password: args.user.password,
-        data: "",
-        Events: "",
+        data: '',
+        Events: '',
       };
       return prisma.createUser(user);
     },
-    deleteUser: async (parent, args) => {
-      return prisma.deleteUser({ id: args.id });
-    },
-    updateUser: async (parent, { id, name, password, data, Events }) => {
-      const user = await prisma.user({ id: id });
+    deleteUser: async (parent, args) => prisma.deleteUser({ id: args.id }),
+    updateUser: async (parent, {
+      id, name, password, data, Events,
+    }) => {
+      const user = await prisma.user({ id });
       const userData = {
-        name: name ? name : user.name,
-        password: password ? password : user.password,
-        data: data ? data : user.data,
-        Events: Events ? Events : user.Events,
+        name: name || user.name,
+        password: password || user.password,
+        data: data || user.data,
+        Events: Events || user.Events,
       };
 
       return prisma.updateUser({
         data: userData,
-        where: { id: id },
+        where: { id },
       });
     },
   },
@@ -48,23 +45,22 @@ const resolvers = {
 
 const server = new ApolloServer({
   typeDefs: gql`
-    ${readFileSync(__dirname.concat("/src/schema.graphql"), "utf8")}
+    ${readFileSync(__dirname.concat('/src/schema.graphql'), 'utf8')}
   `,
   resolvers,
   engine: {
     reportSchema: false,
-    variant: "current",
+    variant: 'current',
   },
 });
 
-server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
-  console.log(`🚀 Server ready at ${url}`);
-});
+// server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
+//   console.log(`🚀 Server ready at ${url}`);
+// });
 
 const app = express();
-app.use(serveStatic(path.join(__dirname, "dist")));
+server.applyMiddleware({ app, path: '/graphql' });
+app.use(serveStatic(path.join(__dirname, 'dist')));
 const port = process.env.PORT || 80;
 console.log(port);
 app.listen(port);
-
-///////////////////////////////////////////////////////////
